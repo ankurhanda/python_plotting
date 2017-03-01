f1dd2a39b37b213cb334d7e78218eb36b5e15a3b
Ankur Handa <handa.ankur@gmail.com>
2017-03-01 22:40:17 +0000

    adding interactive plot
    Signed-off-by: Ankur Handa <handa.ankur@gmail.com>

--- subplot_tighest_interactive.py
+++ subplot_tighest_interactive.py
@@ -11,10 +11,19 @@ fig,ax = plt.subplots(1)
 fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
 
 # Display the image
-ax.imshow(someImage[:,:,0],extent=(0,1,1,0))
+im1 = ax.imshow(someImage[:,:,0],extent=(0,1,1,0))
 
 # Turn off axes and set axes limits
 ax.axis('tight')
 ax.axis('off')
 
-plt.show()
+fig.show();
+for ii in range(1,30):
+    someImage = np.random.random((10,10,1))
+    im1.set_data(someImage[:,:,0]);
+    im1.set_clim(vmin=someImage[:,:,0].min(), vmax=someImage[:,:,0].max())
+    fig.show(); 
+    plt.pause(1.00001);
+
+#plt.show()
+
