23d36864640134fcc4883a59d705df090d3088d8
Ankur Handa <handa.ankur@gmail.com>
2017-03-01 22:35:28 +0000

    upadte
    Signed-off-by: Ankur Handa <handa.ankur@gmail.com>

--- subplot_tighest.py
+++ subplot_tighest.py
@@ -1,29 +1,21 @@
 import matplotlib.pyplot as plt
-from matplotlib import gridspec
 import numpy as np
 
-def do_plot(ax):
-    someImage = np.random.random((10,10,1))
-    ax.imshow(someImage[:,:,0], interpolation='none')
-    ax.set_axis_off()
-    ax.autoscale_view('tight')
+#Random Image
+someImage = np.random.random((10,10,1))
 
-N = 11
-cols = 4
-rows = 4
+# Creare your figure and axes
+fig,ax = plt.subplots(1)
 
-gs = gridspec.GridSpec(rows, cols) 
+# Set whitespace to 0
+fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
 
-fig = plt.figure()
-plt.axis('off')
-plt.subplot_tool()
-plt.autoscale('tight')
-for n in range(N):
-    ax = fig.add_subplot(gs[n])
-    do_plot(ax)
-    plt.tight_layout()
-    fig.show()
-    plt.pause(0.00001);
-    
-plt.pause(1000)
+# Display the image
+ax.imshow(someImage[:,:,0],extent=(0,1,1,0))
+
+# Turn off axes and set axes limits
+ax.axis('tight')
+ax.axis('off')
+
+plt.show()
 
