d903d8267cd79af0d52344f4257f4d7511975968
Ankur Handa <handa.ankur@gmail.com>
2017-03-02 13:27:41 +0000

    adding plotting utility for labels and images
    Signed-off-by: Ankur Handa <handa.ankur@gmail.com>

--- subplot_tightest_interactive_torch_style.py
+++ subplot_tightest_interactive_torch_style.py
@@ -2,15 +2,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import read_sunrgbd_data
+import matplotlib as mpl
 
 
-def tile_images(img, batch_size, rows, cols):
-    batchImages = np.random.random((240*rows,320*cols,3))
+def tile_images(img, batch_size, rows, cols, rgb):
+    
+    batchImages = np.random.random((240*rows,320*cols,rgb))
+
+    if rgb>1:
+        batchImages = np.random.random((240*rows,320*cols,rgb))
+    else:
+        batchImages = np.random.random((240*rows,320*cols))
+        
     for i in range(rows):
         for j in range(cols):
             if i*cols+j < batch_size:
-                batchImages[0+i*240:(i+1)*240,0+j*320:(j+1)*320,:] = img[i*cols+j]
-            
+                if rgb>1:
+                    batchImages[0+i*240:(i+1)*240,0+j*320:(j+1)*320,:] = img[i*cols+j]
+                else:
+                    batchImages[0+i*240:(i+1)*240,0+j*320:(j+1)*320]   = img[i*cols+j]
+           
     return batchImages
         
 
@@ -30,24 +41,51 @@ print('{0}, {1}'.format(rows,cols))
 SUNRGBD_dataset = read_sunrgbd_data.dataset("SUNRGBD","/media/ankur/nnseg/sunrgbd_training.txt")
 img, label = SUNRGBD_dataset.get_random_shuffle(args.batch_size)
 
-batchImages = tile_images(img, args.batch_size, rows, cols)
+#batchImages = tile_images(img, args.batch_size, rows, cols,3)
+batchImages = tile_images(label, args.batch_size, rows, cols,1)
+
+#inspired by http://jdherman.github.io/colormap/
+colour_code = [(0, 0, 0),(0,0,1),(0.9137,0.3490,0.1882), (0, 0.8549, 0),
+               (0.5843,0,0.9412),(0.8706,0.9451,0.0941),(1.0000,0.8078,0.8078),
+               (0,0.8784,0.8980),(0.4157,0.5333,0.8000),(0.4588,0.1137,0.1608),
+               (0.9412,0.1373,0.9216),(0,0.6549,0.6118),(0.9765,0.5451,0),
+               (0.8824,0.8980,0.7608)];
+
+
+cm = mpl.colors.ListedColormap(colour_code)
+
+#{ {0 ,0 ,0},
+#                {0, 0, 1}, --BED
+#                {0.9137,0.3490,0.1882}, --BOOKS
+#                {0, 0.8549, 0}, --CEILING
+#                {0.5843,0,0.9412}, --CHAIR
+#                {0.8706,0.9451,0.0941}, --FLOOR
+#                {1.0000,0.8078,0.8078}, --FURNITURE
+#                {0,0.8784,0.8980}, --OBJECTS
+#                {0.4157,0.5333,0.8000}, --PAINTING
+#                {0.4588,0.1137,0.1608}, --SOFA
+#                {0.9412,0.1373,0.9216}, --TABLE
+#                {0,0.6549,0.6118}, --TV
+#                {0.9765,0.5451,0}, --WALL
+#                {0.8824,0.8980,0.7608} --WINDOW
+#              }
 
 #Random Image
 someImage = np.random.random((240*np.int(rows),320*np.int(cols),3))
 
 # Creare your figure and axes
-fig,ax = plt.subplots(1)
+fig, ax = plt.subplots(1)
 
 # Set whitespace to 0
 fig.subplots_adjust(left=0,right=1,bottom=0,top=1)
 
 # Display the image
-#ax.imshow(someImage[:,:,0],extent=(0,1,1,0))
-ax.imshow(np.uint8(batchImages),extent=(0,1,1,0))
+#ax.imshow(np.uint8(batchImages),extent=(0,1,1,0))
+# Display the labels
+ax.imshow(np.uint8(batchImages),extent=(0,1,1,0),cmap=cm)
 
 # Turn off axes and set axes limits
 ax.axis('tight')
 ax.axis('off')
 
 plt.show()
-
